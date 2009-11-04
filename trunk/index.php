7ffa08e2fd73208e86c3360f1b827d6dc14bd5c9
xami520 <xami520@4dd63d02-fc4e-0410-aa2f-81f071f90814>
2009-11-04 01:59:49 +0000

    正确呈现帖子，网页数据还是服务端取得

--- trunk/index.php
+++ trunk/index.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 <title>Super AJAX Programming Seed v.1.0</title>
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <script type="text/javascript" src="ajax.js"></script>
 <script type="text/javascript">
 
@@ -31,16 +31,5 @@
 	</div>
 </div>
 
-<h2>Super AJAX Programming Seed v.1.0</h2>
-<p>If you've been thinking about starting to program with AJAX, this is a great place to start. Everything you need is contained in three tiny files. This one, the external javascript file that contains the AJAX functions (ajax.js) and the script page that processes the data. Once you get started playing with this, you'll never stop!</p>
-<h4>How does it work?</h4>
-<p>AJAX is the method that you can process data dynamically without refreshing a page, basically making a web page nearly as functional as any desktop application. You can to form validation, manipulate database data, whatever!</p>
-<p>It works by a few little JavaScript functions that replace the content of an HTML element with the content from another URL. So what you do is pass data from the first page to that other URL through the URL query string, like this: <strong>script_page.php?content=this+is+the+content</strong>. On script_page.php, you can process the data any way you want to, and you just echo the result.</p>
-<h4>Some variations on the theme</h4>
-<p>Here are some ideas for things you can do to use this technology even more complete:</p>
-<ul>
-  <li>You can pass additional variables by adjusting the getScriptPage() function on this page to include another variable. Then just add the new variable when you call the script.</li>
-  <li>You can add the getScriptPage() function to any of the JavaScript even handlers like onClick or onMouseOver. You can do some fun stuff by experimenting with these different events.</li>
-</ul>
 </body>
 </html>
