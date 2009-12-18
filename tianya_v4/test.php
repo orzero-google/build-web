2f9706b2bc51cc104ceabdc027b88da95d59b11a
xami520 <xami520@4dd63d02-fc4e-0410-aa2f-81f071f90814>
2009-12-18 09:28:31 +0000

    开始设计内容数据库

--- tianya_v4/test.php
+++ tianya_v4/test.php
@@ -1,6 +1,6 @@
 <?php
 
-$compressed = base64_encode('Compress me');
-echo $compressed;
+//$compressed = base64_encode('Compress me');
+//echo $compressed;
 
-
+echo strlen('tianya/67e5b36192a11a87fd2f94a8ddbe52da/54b4111670cacc38f8b1e61b6eb8a687.php');
